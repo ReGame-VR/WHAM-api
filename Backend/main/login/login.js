f1bb00de376c29afdf4bdc603a6932fc30ee27f6
dajin <dajinchu@gmail.com>
2018-05-29 22:55:54 -0400

    Added different login views

--- Backend/main/login/login.js
+++ Backend/main/login/login.js
@@ -2,7 +2,7 @@
 // Request Response -> Void
 exports.show_login = function(req, res) {
     if (req.accepts('html')) {
-        res.render('login');
+        res.render('login-picker');
     } else {
         res.writeHead(403, {
             'Content-Type': 'application/json',
--- Backend/main/login/patient/patient_login.js
+++ Backend/main/login/patient/patient_login.js
@@ -1,5 +1,5 @@
 // Renders the login page for a patient
 // Request Response -> Void
 exports.show_login = function(req, res) {
-    res.render('login');
+    res.render('login-patient');
 };
